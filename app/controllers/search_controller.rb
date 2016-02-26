6e2f2ecc3578b60fb104995c3f51db34941c2116
jessicaahong <jessiehong9@gmail.com>
2016-02-25 20:39:38 -0800

    finishes up a lot of CSS

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,5 @@
 class SearchController < ApplicationController
 	before_action :require_login
-# Listing.where(city: "San Francisco", produce_category: nil)
 	def index
 		search_term = {}
 		if params[:Cities].nil? || params[:Category].nil?
@@ -14,24 +13,4 @@ class SearchController < ApplicationController
 		@produce_categories = Listing.uniq.pluck(:produce_category)
 		render layout: "search_listings"
 	end
-
-	# def show
-	# 	search_term = {}
-	# 	search_term[:city] = params[:Cities] unless params[:Cities].empty?
-	# 	search_term[:produce_category] = params[:Category] unless params[:Category].empty?
-	# 	@listings = Listing.where(search_term)
-	# 	@cities = Listing.uniq.pluck(:city)
-	# 	@produce_categories = Listing.uniq.pluck(:produce_category)
-	# 	render layout: "search_listings"
-	# end
-
-# 	def searchresults
-# 		@results = Listing.where(city: :option_city, produce_category: :option_category)
-# 		render layout: "search_listings"
-# 	end
-
-# private
-# 	def query_params
-# 		params.require(:listings).permit(:option_city, :option_category)
-# 	end
 end
