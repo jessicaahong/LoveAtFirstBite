3fc7636211f5125b3dcabe22005efacfc9151697
jessicaahong <jessiehong9@gmail.com>
2016-04-28 11:08:34 -0700

    adds AWS path and domain url to config files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,9 @@ Rails.application.configure do
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       s3_region: 'us-west-2'
-    }
+    },
+    :url => ':s3_domain_url',
+    :path => '/:class/:attachment/:id_partition/:style/:filename'
   }
 
 
