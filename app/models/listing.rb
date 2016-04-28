66930998ce36d89f9b228f2646dab2c1ff8b31f4
jessicaahong <jessiehong9@gmail.com>
2016-04-28 10:45:35 -0700

    renames AWS bucket in models

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,7 +5,7 @@ class Listing < ActiveRecord::Base
 					styles: { medium: "300x300>", thumb: "100x100>" }, 
 					default_url: "/images/:style/missing_listing_avatar.png",
 					storage: :s3,
-					bucket: ENV["AWS_S3_BUCKET"],
+					bucket: ENV['S3_BUCKET_NAME'],
 					s3_credentials: {access_key_id: ENV["AWS_ACCESS_KEY_ID"], secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]}
 	# has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing_listing_avatar.png"
 	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
